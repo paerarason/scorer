900dcb40eb6d720ccc15bb16d03118505339b571
paerarason <paerarason1111@gmail.com>
2023-12-19 20:12:29 +0530

    rest

--- scorer/match/MatchHandler.go
+++ scorer/match/MatchHandler.go
@@ -2,6 +2,7 @@ package scorer
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"context"
 	"github.com/paerarason/scorer/database"
 )
 
@@ -11,14 +12,14 @@ import (
 
 func MatchCreate()gin.HandlerFunc {
     return func(c *gin.Context){
-		var mth match
+		var mth database.Match
         //error Handling while Serialize the json from the request to the Account Struct 
         if err:=c.BindJSON(&mth);err!=nil{
              c.JSON(http.StatusBadRequest,gin.H{"message": "Bad Request "}) 
              return 
 		}
 	
-	client, err := database.connectToMongo()
+	client, err := database.ConnectToMongo()
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
@@ -30,4 +31,5 @@ func MatchCreate()gin.HandlerFunc {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
+}
 }
